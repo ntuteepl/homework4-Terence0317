81e501ea51b6e19ea1f935aba4cc1219b6cd4b0c
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-25 20:13:57 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+    int array[n]; // begin from array[0] to array[n-1];
+    if (n){
+        for (int i = 0 ; i < n ; i++) scanf("%d", &array[i]);
+        for (int i = 0 ; i < n ; i++){
+            if (array[i] == 1) printf("NO ");
+            else if (array[i] == 2) printf("YES ");
+            else{
+                for (int j = 2 ; j < array[i] ; j++){
+                    if (array[i] % j == 0){
+                        printf("NO ");
+                        break;
+                    }
+                    else if (array[i] == j+1) printf("YES ");
+                }
+            }
+        }
+    }
+}
 
