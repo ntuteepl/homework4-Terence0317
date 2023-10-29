17da51c668f6f5a44c2290b2730f691f961fe502
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-29 13:11:43 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int n, i, j, k, max,mj,mk;
+    int n, i, j, k, max,mi,mj;
     scanf("%d", &n);
     int array[n][n];
     for (i = 0 ; i < n ; i++) for (j = 0 ; j < n ; j++) scanf("%d", &array[i][j]);
@@ -14,13 +14,13 @@ int main()
             for (j = 0 ; j < n ; j++){
                 if (max <= array[i][j]){
                 mi = i+1;
-                mi = j+1;
+                mj = j+1;
                 max = array[i][j];
                 }
             }
-            printf("\n%s%d%s%d", "boy ", mj, " pair with girl ", mk);
+            printf("\n%s%d%s%d", "boy ", mi, " pair with girl ", mj);
+            for (k = 0 ; k < n ; k++) array[mi][i] = 0;
+            for (k = 0 ; k < n ; k++) array[i][mj] = 0;
         }
-        for (k = 0 ; k < n ; k++) array[mi][i] = 0;
-        for (k = 0 ; k < n ; k++) array[i][mj] = 0;
     }
 }
