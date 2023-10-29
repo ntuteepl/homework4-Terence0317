8ec2740b55b3270e1adc8d6930c44e5e4dfaa234
林佑恩 <146064642+Terence0317@users.noreply.github.com>
2023-10-29 13:10:18 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,44 +1,26 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int n,x,y,max,mx,my,i,j,k,t;
-    scanf("%d",&n);
-    int a[n][n];
-    for (i=0;i<n;i++)
-    {
-        for (j=0;j<n;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(i=0;i<n;i++)
-    {
-            max=a[0][0];
-            for (x=0;x<n;x++)
-            {
-                for (y=0;y<n;y++)
-                {
-                    if (max<=a[x][y])
-                    {
-                        mx=x;
-                        my=y;  // if a[x][y] is max,max[][]=a[mx][my]
-                        max=a[x][y];
-                    }
+    int n, i, j, k, max,mj,mk;
+    scanf("%d", &n);
+    int array[n][n];
+    for (i = 0 ; i < n ; i++) for (j = 0 ; j < n ; j++) scanf("%d", &array[i][j]);
+    for (k = 0 ; k < n ; k++){
+        max = array[0][0];
+        for (i = 0 ; i < n ; i++){
+            for (j = 0 ; j < n ; j++){
+                if (max <= array[i][j]){
+                mi = i+1;
+                mi = j+1;
+                max = array[i][j];
                 }
             }
-
-            printf("boy %d pair with girl %d\n",mx+1,my+1);
-
-            for (k=0;k<n;k++)
-            {
-                a[mx][k]=0;
-            }
-            for (k=0;k<n;k++)
-            {
-                a[k][my]=0;
-            }
-
+            printf("\n%s%d%s%d", "boy ", mj, " pair with girl ", mk);
+        }
+        for (k = 0 ; k < n ; k++) array[mi][i] = 0;
+        for (k = 0 ; k < n ; k++) array[i][mj] = 0;
     }
 }
